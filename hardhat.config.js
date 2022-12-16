719a7d3fa910dae6af5a03e72ebc27128165183f
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2022-12-16 19:47:39 +0530

    Errors solved

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.17",
 };
