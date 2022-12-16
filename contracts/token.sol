4809d49eba92bd0f5599331084255d96071cb0df
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2022-12-16 19:34:03 +0530

    Contract Created

---
+++ contracts/token.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Token{
+    string public name = "Psy Token" ;
+    string public symbol = "PSY";
+    uint public totalsupply = 10000;
+
+    address public owner;
+
+    mapping(address => uint) balances;          //This will tell how many tokens does that account has
+
+    constructor(){
+        balances[msg.sender] = totalSupply ;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint amount) external{
+        require(balances[msg.sender]>=amount,"Not enough Tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+    function balanceof(address account) external view returns (uint){
+        return balances[account];
+    }
+}
