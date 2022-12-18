f0162a22e291be52369aaa06e6555c3dc10aff19
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2022-12-18 11:39:50 +0530

    Another test added for transfer of token

--- test/token.js
+++ test/token.js
@@ -5,14 +5,27 @@ describe("Token contract", function() {
     it("Deployment should assign the total supply of tokens to the owner",async function(){
         const [owner] = await ethers.getSigners();                      //Can access account details through getSigners
 
-        console.log("Signers object:", owner);
         const Token = await ethers.getContractFactory("Token");         //Creates Instance of the contract
 
         const hardhatToken = await Token.deploy();                      //Deployment of that instance in hardhat
 
         const ownerBalance = await hardhatToken.balanceOf(owner.address)//ownerBalance = 10000
-        console.log("Owner Address:", owner.address);
 
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance) //total Supply= 10000(chai library) checking
-    })
-})
+    });
+    it("Should transfer tokens between accounts",async function(){
+        const [owner, add1, add2] = await ethers.getSigners();                      //Can access account details through getSigners
+
+        const Token = await ethers.getContractFactory("Token");         //Creates Instance of the contract
+
+        const hardhatToken = await Token.deploy();                      //Deployment of that instance in hardhat
+        //Transfer 10 tokens from owner to add1
+
+        await hardhatToken.transfer(add1.address, 10);
+        expect(await hardhatToken.balanceOf(add1.address)).to.equal(10);
+
+        //Transfer 5 token from add1 to add2
+        await hardhatToken.connect(add1).transfer(add2.address, 5);
+        expect(await hardhatToken.balanceOf(add2.address)).to.equal(5);
+    });
+});
