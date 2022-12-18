d7a4ded60c0c2daa95a825a8369e62a4bfd4d52d
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2022-12-18 15:10:13 +0530

    Debug

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.9.0;
 
+// import "hardhat/console.sol";                                                    //For Debugging
+
 contract Token{
     string public name = "Psy Token" ;
     string public symbol = "PSY";
@@ -16,10 +18,13 @@ contract Token{
     }
 
     function transfer(address to, uint amount) external{
+        // console.log("**Sender balance is %s tokens**",balance[msg.sender]);         //For Debugging
+        // console.log("**Sender is sending %s token to %s address**",amount,to);      //For Debugging
         require(balances[msg.sender]>=amount,"Not enough Tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
     }
+
     function balanceOf(address account) external view returns (uint){
         return balances[account];
     }
