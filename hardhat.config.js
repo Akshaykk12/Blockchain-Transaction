9b71bfdc43dc190bdac18f6668fcd31c448fe6eb
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2023-01-08 14:01:11 +0530

    Deployed to ploygon Network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
-
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
+const {API_URL, PRIVATE_KEY} = process.env;
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai:{
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
